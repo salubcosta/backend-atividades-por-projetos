0bc3c27345a629ea6796edb927fe36f47c4d799b
Salumao Barbosa da Costa <salubcosta@gmail.com>
2026-03-13 01:17:04 -0300

    refactor(repository): adicionado comentários na classe CategoriaRepository e métodos

--- repositories/categoria_repository.py
+++ repositories/categoria_repository.py
@@ -3,8 +3,24 @@ from database.database import Session
 from models import Categoria
 
 class CategoriaRepository:
+    """
+    Responsável pelo acesso e manipulação dos dados da tabela categoria no BD
+
+    Esta classe encapsula as operações de CRUD
+
+    Repository não contém regras de negócio, apenas operações de banco de dados
+    """
 
     def criar_categoria(self, nome: str) -> Categoria:
+        """
+        Cria uma nova categoria no DB
+
+        Arguments:
+            nome: Nome da categoria
+        
+        Returns:
+            Cateogoria | None: Retorna o objeto categoria ou None caso ocorra algum erro.
+        """
         with Session() as session:
             categoria = Categoria(nome=nome)
             try:
@@ -21,6 +37,16 @@ class CategoriaRepository:
                 return None
         
     def atualizar(self, categoria_id: int,  nome: str):
+        """
+        Atualiza uma categoria no BD
+
+        Arguments:
+            categoria_id: PK da categoria
+            nome: Novo nome da categoria
+            
+        Returns:
+            Cateogoria | None: Retorna o objeto categoria ou None caso ocorra algum erro.
+        """
         with Session() as session:
             categoria = session.query(Categoria).filter(Categoria.id == categoria_id).first()
 
@@ -40,14 +66,40 @@ class CategoriaRepository:
                 return None
 
     def listar_categorias(self):
+        """
+        Responsável por listar todas as categorias
+
+        Return:
+            list[Categoria]: Return uma lista contendo todas as categorias
+        """
         with Session() as session:
             return session.query(Categoria).all()
     
     def buscar_por_id(self, categoria_id: int):
+        """
+        Responsável por buscar categoria por id
+
+        Arguments:
+            categoria_id: PK de categoria
+
+        Return:
+            Categoria: Retorna a primeira ocorrência de categoria por id
+        """
         with Session() as session:
             return session.query(Categoria).filter(Categoria.id == categoria_id).first()
         
     def deletar_categoria(self, categoria_id: int):
+        """
+        Responsável por deletar uma categoria pelo id
+
+        Arguments:
+            categoria_id: PK de categoria
+
+        Return:
+            count: Retona a quantidade de registros afetados. Como estamos deletando por id,
+            caso encontre registro. Retorna 1, caso não encontre, retorna 0. E em contexto
+            booleano, 0 equivale a False. 
+        """
         with Session() as session:
             count = session.query(Categoria).filter(Categoria.id == categoria_id).delete()
             session.commit()
