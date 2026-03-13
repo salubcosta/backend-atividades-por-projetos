b28ea29eb95e525422a3cb23252adcce23f0d000
Salumao Barbosa da Costa <salubcosta@gmail.com>
2026-03-13 01:22:31 -0300

    feat(init): criado arquivo para facilitar importação em outros arquivos, trata o diretorio como módulo

---
+++ schemas/__init__.py
@@ -0,0 +1,20 @@
+from schemas.categoria_schema import (
+    CategoriaCreateSchema,
+    CategoriaUpdateSchema,
+    CategoriaResponseSchema,
+    CategoriaListResponseSchema,
+    CategoriaGetId
+)
+from schemas.projeto_schema import (
+    ProjetoCreateSchema,
+    ProjetoUpdateSchema,
+    ProjetoResponseSchema,
+    ProjetoListResponseSchema,
+    ProjetoGetId
+)
+from schemas.registro_schema import (
+    RegistroCreateSchema,
+    RegistroUpdateSchema,
+    RegistroResponseSchema,
+    RegistroListResponseSchema
+)
